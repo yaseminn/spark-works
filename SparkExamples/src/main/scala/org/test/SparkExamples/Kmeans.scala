ddfd39e9114b0f7473ab3b1225d21fa0b5134b1c
yaseminn <godot85@gmail.com>
2015-11-30 23:08:09 +0200

    kmeans and word count

---
+++ SparkExamples/src/main/scala/org/test/SparkExamples/Kmeans.scala
@@ -0,0 +1,48 @@
+package org.test.SparkExamples
+
+/**
+ * @author yasemin
+ */
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.mllib.clustering.{KMeansModel, KMeans}
+import org.apache.spark.mllib.linalg.Vectors
+
+object Kmeans {
+  def main(args: Array[String]) {
+    if (args.length < 4) {
+      println("usage: <input> <output> <numClusters> <maxIterations>")
+      System.exit(0)
+    }
+
+    val conf = new SparkConf
+    conf.setAppName("Spark KMeans Example").setMaster("local")
+    val context = new SparkContext(conf)
+
+    val input = args(0)
+    val output = args(1)
+    val K = args(2).toInt
+    val maxIteration = args(3).toInt
+    val runs = calculateRuns(args)
+    
+    val data = context.textFile(input).map {
+      line => Vectors.dense(line.split(',').map(_.toDouble))
+    }.cache()
+
+    val clusters: KMeansModel = KMeans.train(data, K, maxIteration, runs)
+    println("cluster centers: " + clusters.clusterCenters.mkString(","))
+
+    val vectorsAndClusterIdx = data.map{ point =>
+      val prediction = clusters.predict(point)
+      (point.toString, prediction)
+    }
+
+    vectorsAndClusterIdx.saveAsTextFile(output)
+
+    context.stop()
+  }
+
+  def calculateRuns(args: Array[String]): Int = {
+    if (args.length > 4) args(4).toInt
+    else 1
+  }
+}
--- SparkExamples/src/main/scala/org/test/SparkExamples/WordCount.scala
+++ SparkExamples/src/main/scala/org/test/SparkExamples/WordCount.scala
@@ -1,8 +1,37 @@
 package org.test.SparkExamples
 
 /**
- * @author inosens
+ * @author yasemin
  */
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
 object WordCount {
-  
+  def main(args: Array[String]) = {
+     if (args.length < 2) {
+      println("usage: <input> <output>")
+      System.exit(0)
+    }
+    
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local")
+
+    val sc = new SparkContext(conf)
+
+    val input = args(0)
+    val output = args(1)
+    
+    val text = sc.textFile(input)
+    text.flatMap { line =>
+      line.split(" ")
+    }
+      .map { word =>
+        (word, 1)
+      }
+    .reduceByKey(_+_)
+      .saveAsTextFile(output)
+    
+
+  }
 }
